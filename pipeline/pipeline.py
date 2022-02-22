56330ccb25caafff9685e2c3572e389eeb825409
ShearmanChua <shearman_chua@hotmail.com>
2022-02-22 12:19:27 +0800

    new

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -4,4 +4,41 @@ PIPELINE_PROJECT_NAME = "ontonotes pipeline"
 PIPELINE_NAME = "pipeline task"
 TASK_PROJECT_NAME = "ontonotes"
 
-#
+pipe = PipelineController(
+    project=PIPELINE_PROJECT_NAME,
+    name=PIPELINE_NAME,
+    version="0.1",
+    add_pipeline_tags=True,
+)
+
+pipe.set_default_execution_queue("compute2")  # set to queue with GPU
+
+pipe.add_step(
+    name="dataset_parsing_to_json",
+    base_task_project=TASK_PROJECT_NAME,
+    base_task_name="dataset_parsing_to_json",
+    parameter_override={"General/source_tar": "ontonotes tar"},
+)
+pipe.add_step(
+    name="retrieve unique NER tags",
+    parents=["dataset_parsing_to_json"],
+    base_task_project=TASK_PROJECT_NAME,
+    base_task_name="retrieve unique NER tags",
+    parameter_override={
+        "General/json_dataset": "ontonotes json"
+    },
+)
+pipe.add_step(
+    name="model_training",
+    parents=["retrieve unique NER tags"],
+    base_task_project=TASK_PROJECT_NAME,
+    base_task_name="model_training",
+    parameter_override={
+        "General/json_dataset": "ontonotes json"
+    },
+)
+
+# Starting the pipeline (in the background)
+pipe.start()
+
+print("done")
