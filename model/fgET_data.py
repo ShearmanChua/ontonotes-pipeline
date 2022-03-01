aa417a1a52b3c20705119b839845723aa58a8ec3
ShearmanChua <shearman_chua@hotmail.com>
2022-03-01 15:27:14 +0800

    new

---
+++ model/fgET_data.py
@@ -0,0 +1,48 @@
+import json
+import re
+import torch
+import model.constant as C
+from torch.utils.data import Dataset
+from allennlp.modules.elmo import batch_to_ids
+import dask.dataframe as dd
+
+def bio_to_bioes(labels):
+    """Convert a sequence of BIO labels to BIOES labels.
+    :param labels: A list of labels.
+    :return: A list of converted labels.
+    """
+    label_len = len(labels)
+    labels_bioes = []
+    for idx, label in enumerate(labels):
+        next_label = labels[idx + 1] if idx < label_len - 1 else 'O'
+        if label == 'O':
+            labels_bioes.append('O')
+        elif label.startswith('B-'):
+            if next_label.startswith('I-'):
+                labels_bioes.append(label)
+            else:
+                labels_bioes.append('S-' + label[2:])
+        else:
+            if next_label.startswith('I-'):
+                labels_bioes.append(label)
+            else:
+                labels_bioes.append('E-' + label[2:])
+    return labels_bioes
+
+
+def mask_to_distance(mask, mask_len, decay=.1):
+    if 1 not in mask:
+        return [0] * mask_len
+    start = mask.index(1)
+    end = mask_len - list(reversed(mask)).index(1)
+    dist = [0] * mask_len
+    for i in range(start):
+        dist[i] = max(0, 1 - (start - i - 1) * decay)
+    for i in range(end, mask_len):
+        dist[i] = max(0, 1 - (i - end) * decay)
+    return dist
+
+class FetDataset(Dataset):
+    def __init__(self, gpu=False):
+        self.gpu = gpu
+        self.data = dd.read_parquet(training_file,engine='fastparquet')
--- pipeline/ontonotes_to_json.py
+++ pipeline/ontonotes_to_json.py
@@ -25,7 +25,7 @@ def ontonotes_to_json():
     task.set_base_docker("nvidia/cuda:11.4.0-cudnn8-devel-ubuntu20.04")
     args = {"dst_file":DESTINATION_FILE_NAME,"language":LANGUAGE,"project":PROJECT_NAME,"source_tar":TAR_PARTIAL_NAME,"index":INDEX_PARTIAL_NAME}
     task.connect(args)
-    task.execute_remotely()
+    task.execute_remotely(queue_name='compute2')
 
     # get tar datset uploaded
     tar_dataset_dict = Dataset.list_datasets(
