72e12df1495396559872c040b2e72260ecf45184
Shearman Chua <48722169+ShearmanChua@users.noreply.github.com>
2022-03-11 21:53:14 +0800

    new

--- pipeline/fgETPipelineTasks/HAnDS_1mil.py
+++ pipeline/fgETPipelineTasks/HAnDS_1mil.py
@@ -79,9 +79,9 @@ def HAnDS_1mil():
     print("val df:", val_df)
     print("test df:", test_df)
 
-    train_df.drop(train_df[train_df.fine_grained_entities == []].index, inplace=True)
-    val_df.drop(val_df[val_df.fine_grained_entities == []].index, inplace=True)
-    test_df.drop(test_df[test_df.fine_grained_entities == []].index, inplace=True)
+    train_df.drop(train_df[train_df["fine_grained_entities"].str.len() == 0].index, inplace=True)
+    val_df.drop(val_df[val_df["fine_grained_entities"].str.len() == 0].index, inplace=True)
+    test_df.drop(test_df[test_df["fine_grained_entities"].str.len() == 0].index, inplace=True)
 
     print("new train df:", train_df)
     print("new val df:", val_df)
