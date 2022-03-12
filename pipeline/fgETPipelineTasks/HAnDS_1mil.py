82538f93b2d7fc2005c68166380964e2bf99d7e3
ShearmanChua <shearman_chua@hotmail.com>
2022-03-12 12:15:29 +0800

    new

--- pipeline/fgETPipelineTasks/HAnDS_1mil.py
+++ pipeline/fgETPipelineTasks/HAnDS_1mil.py
@@ -11,13 +11,16 @@ import codecs
 import re
 import numpy as np
 import gzip
+import model.constant as C
+
+from allennlp.modules.elmo import batch_to_ids
 
 def HAnDS_1mil():
     PROJECT_NAME = "fgET"
     TASK_NAME = "HAnDS_dataset_generate_1mil"
     DATASET_PROJECT = "datasets/multimodal"
     DATASET_PARTIAL_NAME = "fgET HAnDS data"
-    DATASET_NAME = "fgET HAnDS 100k"
+    DATASET_NAME = "fgET HAnDS 100k preprocessed"
 
     # Task.add_requirements("-rrequirements.txt")
     Task.force_requirements_env_freeze(force=True, requirements_file='requirements.txt')
@@ -51,7 +54,6 @@ def HAnDS_1mil():
 
     files_required = []
     
-
     for i in range(0,20):
         files_required.append('train_{}.parquet'.format(str(i)))
         files_required.append('validation_{}.parquet'.format(str(i)))
@@ -86,8 +88,11 @@ def HAnDS_1mil():
     train_df = train_df.sample(n=100000)
     val_df = val_df.sample(n=20000)
     test_df = test_df.sample(n=20000)
-    
 
+    train_df['instance'] = train_df['fine_grained_entities'].apply(process_instance)
+    val_df['instance'] = val_df['fine_grained_entities'].apply(process_instance)
+    test_df['instance'] = test_df['fine_grained_entities'].apply(process_instance)
+    
     print("new train df:", train_df)
     print("new val df:", val_df)
     print("new test df:", test_df)
@@ -102,6 +107,62 @@ def HAnDS_1mil():
 
     dataset.upload(output_url='s3://experiment-logging/multimodal')
     dataset.finalize()
+
+def process_instance(inst):
+    labels_file_path = get_clearml_file_path('datasets/multimodal','fgET data','ner_tags.json')
+
+    with open(labels_file_path) as json_file:
+        label_stoi = json.load(json_file)
+
+    label_size = len(label_stoi)
+    print('Label size: {}'.format(len(label_stoi)))
+
+    tokens = inst['tokens']
+    tokens = [C.TOK_REPLACEMENT.get(t, t) for t in tokens]
+    seq_len = len(tokens)
+    char_ids = batch_to_ids([tokens])[0].tolist()
+    labels_nbz, men_mask, ctx_mask, men_ids, mentions = [], [], [], [], []
+    annotations = inst['entities']
+    anno_num = len(annotations)
+    for annotation in annotations:
+        mention_id = annotation['mention_id']
+        labels = annotation['labels']
+        labels = [l.replace('geograpy', 'geography') for l in labels]
+        start = annotation['start']
+        end = annotation['end']
+
+        men_ids.append(mention_id)
+        mentions.append(annotation['mention'])
+        labels = [label_stoi[l] for l in labels if l in label_stoi]
+        labels_nbz.append(labels)
+        men_mask.append([1 if i >= start and i < end else 0
+                            for i in range(seq_len)])
+        ctx_mask.append([1 if i < start or i >= end else 0
+                            for i in range(seq_len)])
+    return (char_ids, labels_nbz, men_mask, ctx_mask, men_ids, mentions, anno_num,
+            seq_len)
+
+def get_clearml_file_path(dataset_project,dataset_name,file_name):
+
+    # get uploaded dataset from clearML
+    dataset_dict = Dataset.list_datasets(
+        dataset_project=dataset_project, partial_name=dataset_name, only_completed=False
+    )
+
+    datasets_obj = [
+        Dataset.get(dataset_id=dataset_dict["id"]) for dataset_dict in dataset_dict
+    ]
+
+    # reverse list due to child-parent dependency, and get the first dataset_obj
+    dataset_obj = datasets_obj[::-1][0]
+    
+    folder = dataset_obj.get_local_copy()
+
+    file = [file for file in dataset_obj.list_files() if file==file_name][0]
+
+    file_path = folder + "/" + file
+
+    return file_path
 
 if __name__ == '__main__':
     HAnDS_1mil()
