2ee2f5485f81f0d7eaf7cd0138d208ea7e1be1bd
ShearmanChua <shearman_chua@hotmail.com>
2022-03-25 15:52:11 +0800

    refractored elmo out of model

--- pipeline/fgETPipelineTasks/model_training.py
+++ pipeline/fgETPipelineTasks/model_training.py
@@ -545,7 +545,7 @@ def _get_last_child_dataset(dataset_project, dataset_name):
 if __name__ == '__main__':
     from torch.multiprocessing import Pool, Process, set_start_method
     try:
-        set_start_method('fork')
+        set_start_method('spawn')
     except RuntimeError:
         pass
     model_training()
