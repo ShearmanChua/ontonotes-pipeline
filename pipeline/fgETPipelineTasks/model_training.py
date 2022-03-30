608306d6f17ae737af1431ac27ab31ce212d6a70
ShearmanChua <shearman_chua@hotmail.com>
2022-03-30 10:10:03 +0800

    new

--- pipeline/fgETPipelineTasks/model_training.py
+++ pipeline/fgETPipelineTasks/model_training.py
@@ -16,7 +16,7 @@ from clearml import Task, Dataset
 def model_training():
 
     PROJECT_NAME = "fgET"
-    TASK_NAME = "model_training"
+    TASK_NAME = "model_testing"
 
     Task.force_requirements_env_freeze(force=True, requirements_file='requirements.txt')
     Task.add_requirements("torch")
@@ -49,7 +49,7 @@ def model_training():
     arg_parser.add_argument('--labels_dataset_name', type=str, default='fgET HAnDS 1k manual verified preprocessed')
     arg_parser.add_argument('--labels_file_name', type=str, default='ner_tags.json')
     arg_parser.add_argument('--fgETdata_dataset_project', type=str, default='datasets/multimodal')
-    arg_parser.add_argument('--fgETdata_dataset_name', type=str, default='fgET HAnDS 500k preprocessed')
+    arg_parser.add_argument('--fgETdata_dataset_name', type=str, default='fgET HAnDS 1k manual verified preprocessed')
     arg_parser.add_argument('--train_file_name', type=str, default='train.parquet')
     arg_parser.add_argument('--val_file_name', type=str, default='validation.parquet')
     arg_parser.add_argument('--test_file_name', type=str, default='test.parquet')
