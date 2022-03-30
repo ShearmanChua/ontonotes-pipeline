19931abf9f6cd8074fee7e06f0c839183411056a
ShearmanChua <shearman_chua@hotmail.com>
2022-03-30 23:26:31 +0800

    new

--- pipeline/fgETPipelineTasks/model_training.py
+++ pipeline/fgETPipelineTasks/model_training.py
@@ -20,6 +20,8 @@ def model_training():
 
     Task.force_requirements_env_freeze(force=True, requirements_file='requirements.txt')
     Task.add_requirements("torch")
+    Task.add_requirements("python-Levenshtein==0.12.2")
+    Task.add_requirements("jsonnet")
     task = Task.init(project_name=PROJECT_NAME, task_name=TASK_NAME)
     # task.set_base_docker("nvcr.io/nvidia/pytorch:20.08-py3")
     task.set_base_docker("nvidia/cuda:11.4.0-cudnn8-devel-ubuntu20.04")
